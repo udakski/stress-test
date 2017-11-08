61a590e82c854f0b419a00f2c167d95c25b85397
udakski <davey.kingston@gmail.com>
2017-11-08 08:06:25 +0100

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { HttpModule } from '@angular/http';
 
+import {MatRadioModule, MatToolbarModule} from '@angular/material'
+import {MatStepperModule} from '@angular/material'
+
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './components/question/question.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MatRadioModule, MatStepperModule,MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
